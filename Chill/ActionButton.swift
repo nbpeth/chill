62ff86b2ae7925ab708b59646a9147392c0466aa
bud manstrong <nbpeth@gmail.com>
2016-01-16 19:06:16 -0600

    remove default color set on action button

--- Chill/ActionButton.swift
+++ Chill/ActionButton.swift
@@ -3,7 +3,7 @@ import UIKit
 
 @IBDesignable class ActionButton: UIControl {
     
-    @IBInspectable var color:UIColor
+    @IBInspectable var color:UIColor?
     
     override func drawRect(rect: CGRect) {
         var recty = CGRectMake(0,0,rect.height/2,rect.width/2)
@@ -11,19 +11,17 @@ import UIKit
         recty.offsetInPlace(dx: center.x/2, dy: center.y/2)
         let path = UIBezierPath(ovalInRect: rect)
         
-        color.setFill()
+        color?.setFill()
         
         path.fill()
         
     }
     
     override init(frame: CGRect) {
-        self.color = Colors.cobaltSemiTrans
         super.init(frame: frame)
     }
 
     required init?(coder aDecoder: NSCoder) {
-        self.color = Colors.cobaltSemiTrans
         super.init(coder: aDecoder)
 
     }
